55392d928c1ff58bae3ca01ed8b60dc688f4f007
OutroKye <1059684862@qq.com>
2023-10-03 15:16:11 +0800

    add

--- test.py
+++ test.py
@@ -5,3 +5,7 @@ cmd_str = "ros2 service call /map_server/load_map nav2_msgs/srv/LoadMap '{map_ur
 subprocess.run(cmd_str, shell=True)
 
 # ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$HOME/map.yaml
+# ros2 service call /map_server/load_map nav2_msgs/srv/LoadMap '{map_url: /home/keyu/map2.yaml}'
+# ros2 service call /delete_entity gazebo_msgs/srv/DeleteEntity "{name: 'turtlebot3_burger'}"
+# ros2 service call /local_costmap/clear_entirely_local_costmap nav2_msgs/srv/ClearEntireCostmap "{request: {}}"
+# ros2 service call /global_costmap/clear_entirely_global_costmap nav2_msgs/srv/ClearEntireCostmap "{request: {}}"
